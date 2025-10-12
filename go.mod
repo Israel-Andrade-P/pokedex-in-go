f1cdf5705e32c0c0b218a6978fe395065335a480
zel <isral888andrade@gmail.com>
2025-10-12 08:27:29 -0300

    project setup

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Israel-Andrade-P/pokedex-in-go.git
+
+go 1.25.1
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("You brought him here to kill me me me me")
+}
