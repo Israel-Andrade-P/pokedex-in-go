de5d17ade955eec5c16f8d22ae549231c4be613f
zel <isral888andrade@gmail.com>
2025-10-12 18:22:04 -0300

    add help and exit commands

--- main.go
+++ main.go
@@ -3,11 +3,33 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commands map[string]cliCommand
+
 func main() {
+	commands = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    helpCommand,
+		},
+	}
+
 	sc := bufio.NewScanner(os.Stdin)
 	//Drop the step by step again on ChatGPT to clear things up
 
@@ -24,10 +46,37 @@ func main() {
 			continue
 		}
 
-		fmt.Printf("Your command was: %s\n", words[0])
+		validCommand := false
+
+		for key, val := range commands {
+			if words[0] == key {
+				validCommand = true
+				if err := val.callback(); err != nil {
+					log.Fatalf("Error has occurred: ERR %v", err)
+				}
+			}
+		}
+		if !validCommand {
+			fmt.Println("Unknown command")
+		}
 	}
 }
 
 func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(text))
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func helpCommand() error {
+	fmt.Println("Welcome to the Pokedex!\nUsage:")
+	for key, val := range commands {
+		fmt.Printf("%s: %s\n", key, val.description)
+	}
+
+	return nil
 }
